84c9ad7cb3329dde84d36276bf4b8ba3b7f58739
designerinnovation <fullhutchdesignerinnovation@gmail.com>
2016-08-01 20:04:47 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Designer Innovation</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="plugins/font-awesome-4.6.3/css/font-awesome.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/scripts.js"></script>
+	<script src="js/parallax.min.js"></script>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet'>
+	<link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+	<link rel="icon" type="img/png" href="img/logo.png">
+</head>
+
+<body data-spy="scroll" data-target=".navbar">
+
+	<div class="loader-1"><div class="loader"></div></div>
+
+	<div id="top"></div>
+
+	<?php require 'menu.php' ?>
+
+	<!--Header Text-->
+	<header data-parallax="scroll" data-image-src="../designerinnovation/img/backgroundartwork-HQ.png" data-speed=".5">
+			<div class="header-content">
+					<div class="header-content-inner">
+							<h1>Innovation</h1>
+							<p>Turn Your Spark Into a Flame</p>
+							<a href="#portfolio" class="iocDown" title="Learn More"><i class="fa fa-chevron-circle-down fa-5x" aria-hidden="true"></i></a>
+					</div>
+			</div>
+	</header>
+
+	<section id="portfolio">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-12 text-center">
+					<img src="../designerinnovation/img/logo.png" alt="Designer Innovation" height="100">
+					<h2 class="section-heading">Designer Innovation</h2>
+					<h3 class="section-subheading text-muted">Our mission is to turn dreams into reality.</h3>
+				</div>
+			</div>
+			<div class="row">
+					<p class="caption">Abe Lincoln</p>
+					<div class="image-block col-sm-6 hideme" style="background: url(../designerinnovation/img/abe.jpg) no-repeat center top;background-size:cover;" data-fade-speed="50">
+					</div>
+				<div class="image-block col-sm-6 hideme" style="background: url(../designerinnovation/img/george.jpg) no-repeat center top;background-size:cover;" data-fade-speed="200">
+					<p>Abe Lincoln</p>
+				</div>
+				<div class="image-block col-sm-2 hideme" style="background: url(../designerinnovation/img/george.jpg) no-repeat center top;background-size:cover;" data-fade-speed="200">
+					<p>Abe Lincoln</p>
+				</div>
+				<div class="image-block col-sm-6 hideme" style="background: url(../designerinnovation/img/abe.jpg) no-repeat center top;background-size:cover;" data-fade-speed="300">
+					<p>Abe Lincoln</p>
+				</div>
+				<div class="image-block col-sm-4 hideme" style="background: url(../designerinnovation/img/george.jpg) no-repeat center top;background-size:cover;" data-fade-speed="400">
+					<p>Abe Lincoln</p>
+				</div>
+				<div class="image-block col-sm-12 hideme" style="background: url(../designerinnovation/img/abe.jpg) no-repeat center top;background-size:cover;" data-fade-speed="500">
+					<p>Abe Lincoln</p>
+				</div>
+			</div>
+	</section>
+
+	<section id="services">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 text-center">
+					<h2 class="section-heading">What We Do</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-6 hideme" data-fade-speed="200">
+					<img class="img-responsive img-circle" alt="Web Design" src="../designerinnovation/img/statlib.jpg">
+					<h4 class= "section-heading text-center">Web Design</h1>
+					<p class="text-justify">We design mobile friendly websites for the 21st Century. From the simple to the simply elegant, we can do it all.
+					Not only can we offer advanced skills in web design through custom jQuery scripting and CSS styles, but we can offer professional design capabilities
+					for crafting custom logos and artwork using vector graphics and 3D rendering software.</p>
+				</div>
+				<div class="col-sm-6 hideme" data-fade-speed="400">
+					<img class="img-responsive img-circle" alt="Software Development" src="../designerinnovation/img/abe.jpg">
+					<h4 class= "section-heading text-center">Software Development</h1>
+					<p class="text-justify">Let us take bring your ideas to fruition with our development skills. We can bring any idea to life through mobile development, from simple sprite based games and basic touch-button systems
+					to fully interactive 3D games. We can also improve your business' efficiency and productivity with desktop software, from database driven software that can help keep track of inventory and clients that
+					interact with an e-commerce website to time card management software.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<div class="container-fluid" id="transition">
+		<div class="row">
+			<div class="col-lg-12 text-center" data-parallax="scroll" data-image-src="../designerinnovation/img/starbackground.jpg" data-speed=".3">
+				<img src="../designerinnovation/img/logo.png" alt="Designer Innovation" height="200">
+				<h1>Websites that captivate</h1>
+			</div>
+		</div>
+	</div>
+
+	<section id="team">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 text-center">
+					<h2 class="section-heading">The Team</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-6 hideme" data-fade-speed="200">
+					<img class="img-responsive img-circle" alt="Web Design" src="../designerinnovation/img/statlib.jpg" height="100">
+					<h4 class= "section-heading text-center">Ben Fuller</h1>
+					<p class="text-justify">Ben has been working in web design for 7 years and has been programming for 9. He takes his programming skills to the web by handling
+					the majority of the jQuery and server side scripting. He also has a particular interest in graphic design, particularly 3D design, and he uses these skills to create
+					beautiful 3D artwork, much of which can be seen on this website. He also handles most of the desktop software, as he often dreams in C#.</p>
+
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<ul class="social-network social-circle">
+								<li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 hideme" data-fade-speed="300">
+					<img class="img-responsive img-circle" alt="Web Design" src="../designerinnovation/img/statlib.jpg" height="100">
+					<h4 class= "section-heading text-center">Tyler Hutchinson</h1>
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+						<div class="row">
+							<div class="col-sm-12 text-center">
+								<ul class="social-network social-circle">
+									<li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+								</ul>
+							</div>
+						</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<div class="container-fluid" id="transition-1" data-parallax="scroll" data-image-src="../designerinnovation/img/Artwork1.png" data-speed=".3"></div>
+
+	<section id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h2 class="section-heading text-center">Contact us!</h2>
+					<h4 class="section-heading text-center">designerinnovation@designerinnovation.com</h4>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12 text-center">
+					<ul class="social-network social-circle">
+						<li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#" class="icoGoogle" title="Google+"><i class="fa fa-google-plus"></i></a></li>
+						<li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<p>&copy 2016 Designer Innovation</p>
+			</div>
+		</div>
+	</section>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,21 @@
+<?php echo
+'<div class="container-fluid">
+  <nav class="navbar navbar-default navbar-fixed-top">
+      <a class="navbar-brand" href="#footer">Designer Innovation</a>
+      <div class="navbar-header page-scroll">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      <ul class="nav navbar-nav">
+        <li><a href="#top">Home</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#team">The Team</a></li>
+      </ul>
+    </div>
+    </div>
+  </nav>
+</div>';
+?>
